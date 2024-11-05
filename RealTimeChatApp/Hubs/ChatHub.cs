7f324f280de3340a424d9ccf6a760884ef1d7ea9
bls-gelistirici-1 <bls.yazilim.2@hotmail.com>
2024-11-05 15:25:37 +0300

    ChatHub içerisine OnDisconnectedAsync ve OnConnectedAsync eklendi. Aktif kullanıcıların görüntülendiği alan tasarlandı.

--- RealTimeChatApp/Hubs/ChatHub.cs
+++ RealTimeChatApp/Hubs/ChatHub.cs
@@ -4,6 +4,23 @@ namespace RealTimeChatApp.Hubs
 {
     public class ChatHub : Hub
     {
+        private static List<string> _connectedUsers = new List<string>();
+        public override Task OnConnectedAsync()
+        {
+            _connectedUsers.Add(Context.User.Identity.Name);
+
+            Clients.All.SendAsync("UpdateUserList", _connectedUsers);
+
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            _connectedUsers.Remove(Context.User.Identity.Name);
+
+            Clients.All.SendAsync("UpdateUserList", _connectedUsers);
+
+            return base.OnDisconnectedAsync(exception);
+        }
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
